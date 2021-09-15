54d719c78c025b0232be1f5118904ad7a349b03f
francotz123 <41840439+francotz123@users.noreply.github.com>
2021-09-15 18:54:46 -0300

    Update game.js

--- src/js/game.js
+++ src/js/game.js
@@ -177,7 +177,7 @@ class Game {
               i2 = posicion2[0];
               j2 = posicion2[2]; 
             
-              console.log(posicion1,posicion2,i,j,"-",i2,j2,(i + j) % 2 == 0,(i2 + j2) % 2 == 0);
+              console.log(posicion1,posicion2,i,j,"-",i2,j2,(i + j) % 2,(i2 + j2) % 2,(i + j) % 2 == 0,(i2 + j2) % 2 == 0);
              if((i + j) % 2 == 0) {
               $(`#${posicionAnteriorAux}`).css("background-color", `${theme.light}`);
             }else {
